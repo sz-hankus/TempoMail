71fc9c15a76cbecd144f7952cfd739b55cd17c15
AJaszcz <aj303181@student.polsl.pl>
2023-06-21 23:18:45 +0200

    Update .gitignore and html
    Begin makeover of html for css :)

--- src/TempoMail/config.py
+++ src/TempoMail/config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 USER_LIFETIME = int(getenv('ADDRESS_LIFETIME', '3600'))
-ADDRESS_LIFETIME = int(getenv('ADDRESS_LIFETIME', '10'))
+ADDRESS_LIFETIME = int(getenv('ADDRESS_LIFETIME', '600'))
 DEBUG = True if getenv('DEUBG', 'true').lower() == 'true' else False
 REDIS_HOST = getenv('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = getenv('REDIS_PORT', '6379')
