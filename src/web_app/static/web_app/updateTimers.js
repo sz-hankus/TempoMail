c9ce17e1300663e38ffe1b473ccfccbc688f65bb
Szymon Hankus <dephan@protonmail.com>
2023-06-22 16:14:51 +0200

    Added timers
    - Updated the models
    - Added a simple JS script to home.html

---
+++ src/web_app/static/web_app/updateTimers.js
@@ -0,0 +1,19 @@
+function updateTimers() {
+    let now = new Date().getTime();
+    let timers = document.getElementsByClassName("timer");
+    Array.prototype.forEach.call(timers, element => {
+        // JS tmstmp is in milliseconds
+        let tmstmp = parseFloat(element.getAttribute("data-expiryTmstmp")) * 1000
+        let expiryTime = new Date(tmstmp).getTime();
+
+        let diff = Math.floor((tmstmp - now) / 1000)
+        let minutesLeft = Math.floor(diff / 60);
+        let secondsLeft = diff % 60;
+        if (minutesLeft == 0 && secondsLeft == 0)
+            element.parentElement.parentElement.remove();
+        element.children.namedItem("minutes").innerText = minutesLeft.toString().padStart(2, '0');
+        element.children.namedItem("seconds").innerText = secondsLeft.toString().padStart(2, '0');
+    });
+}
+updateTimers()
+setInterval(updateTimers, 1000);
