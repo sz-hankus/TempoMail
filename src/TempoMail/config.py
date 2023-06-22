8cb119bb200bcd2b15fce1aec2f27dc96d9e9b65
Szymon Hankus <dephan@protonmail.com>
2023-06-22 18:24:52 +0200

    Fixed environmental variables

--- src/TempoMail/config.py
+++ src/TempoMail/config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-USER_LIFETIME = int(getenv('ADDRESS_LIFETIME', '3600'))
+USER_LIFETIME = int(getenv('USER_LIFETIME', '3600'))
 ADDRESS_LIFETIME = int(getenv('ADDRESS_LIFETIME', '600'))
 DEBUG = True if getenv('DEUBG', 'true').lower() == 'true' else False
 REDIS_HOST = getenv('REDIS_HOST', '127.0.0.1')
